8c50a6b2a1c05500d77eafe2afd9ccd36dcebdd7
Emmanuel Onyo <emmanuelonyo34@gmail.com>
2023-01-09 23:34:14 +0100

    cors updated

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
-
+const cors = require('cors')
 const app = express();
 
 // Enable body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors({
+    origin: '*'
+}))
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
